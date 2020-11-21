207db177ac6967283ab1e06aac5f4c236f649e16
490513975@qq.com <490513975@qq.com>
2020-11-22 00:10:18 +0800

    修改注释

--- laravel/app/Utils/ZipFile.php
+++ laravel/app/Utils/ZipFile.php
@@ -62,28 +62,27 @@ class ZipFile
     }
 
     /**
-     * 清空/删除 文件夹
+     * 清空文件夹（不会删除当前文件夹）
      * @param string $dirname 文件夹路径
-     * @param bool $self 是否删除当前文件夹
      * @return bool
      */
     function deldir($path)
     {
-        //如果是目录则继续
+        // 如果是目录则继续
         if (is_dir($path)) {
-            //扫描一个文件夹内的所有文件夹和文件并返回数组
+            // 扫描一个文件夹内的所有文件夹和文件并返回数组
             $p = scandir($path);
             foreach ($p as $val) {
-                //排除目录中的.和..
+                // 排除目录中的.和..
                 if ($val != "." && $val != "..") {
-                    //如果是目录则递归子目录，继续操作
+                    // 如果是目录则递归子目录，继续操作
                     if (is_dir($path . $val)) {
-                        //子目录中操作删除文件夹和文件
+                        // 子目录中操作删除文件夹和文件
                         $this->deldir($path . $val . '/');
-                        //目录清空后删除空文件夹
+                        // 目录清空后删除空文件夹
                         @rmdir($path . $val . '/');
                     } else {
-                        //如果是文件直接删除
+                        // 如果是文件直接删除
                         unlink($path . $val);
                     }
                 }
