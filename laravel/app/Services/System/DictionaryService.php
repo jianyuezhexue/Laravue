f177aa52919ccb23ec1aa8b8cca39a33312ce75f
490513975@qq.com <490513975@qq.com>
2020-11-21 23:09:58 +0800

    数据字典删除同时删除子集

--- laravel/app/Services/System/DictionaryService.php
+++ laravel/app/Services/System/DictionaryService.php
@@ -4,20 +4,24 @@ namespace App\Services\System;
 
 use App\Services\Service;
 use App\Utils\ResultHelper;
+use Illuminate\Support\Facades\DB;
 use App\Models\System\DictionaryModel;
+use App\Models\System\DictionaryDetailModel;
 use Symfony\Component\HttpFoundation\Response;
 
 class DictionaryService extends Service
 {
     use ResultHelper;
     protected $model;
+    protected $dictionaryDetailModel;
 
     // 模糊查询
     protected $likeSearch = "desc";
 
-    public function __construct(DictionaryModel $model)
+    public function __construct(DictionaryModel $model, DictionaryDetailModel $dictionaryDetailModel)
     {
         $this->model = $model;
+        $this->dictionaryDetailModel = $dictionaryDetailModel;
     }
 
     /**
@@ -35,6 +39,28 @@ class DictionaryService extends Service
             $result = $this->success(Response::HTTP_OK, '获取全部数据成功！', ["resysDictionary" => $result]);
         } catch (\Exception $ex) {
             $result = $this->failed(Response::HTTP_INTERNAL_SERVER_ERROR, $ex->getMessage());
+        }
+        return $result;
+    }
+
+    /**
+     * 指定ID删除数据连带子集中的数据
+     * @param string $id
+     * @return ResultHelper
+     */
+    public function destroy($id)
+    {
+        DB::beginTransaction();
+        try {
+            // 删除父级数据
+            $result = $this->model->destroy($id);
+            // 删除子集中的数据
+            $result = $this->dictionaryDetailModel->where('sys_dictionary_id', $id)->delete();
+            $result = $this->success(Response::HTTP_OK, '删除数据成功', $result);
+            DB::commit();
+        } catch (\Exception $ex) {
+            $result = $this->failed(Response::HTTP_INTERNAL_SERVER_ERROR, $ex->getMessage());
+            DB::rollBack();
         }
         return $result;
     }
